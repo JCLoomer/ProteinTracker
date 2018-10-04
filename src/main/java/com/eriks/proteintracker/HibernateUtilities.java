92dabe6c840e7d2f53193ee23734c5599a16c51d
jimmy <jimmy@DESKTOP-1RHTEOG>
2018-10-04 23:36:12 +0100

    Configured hibernate config and created a utility program for creating a session factory.

---
+++ src/main/java/com/eriks/proteintracker/HibernateUtilities.java
@@ -0,0 +1,28 @@
+
+package com.eriks.proteintracker;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtilities {
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+    
+    static{
+        try{
+            Configuration configuration = new Configuration().configure();
+            
+            serviceRegistry  = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        catch(HibernateException ex){
+            System.out.println("Error creating session factory");
+        }
+    }
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+}
